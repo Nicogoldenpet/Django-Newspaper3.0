d7c73c84a70b1c086d446f6b7f8840cf476299a7
Nicogoldenpet <nicolasagamezmelo1@gmail.com>
2025-05-18 12:33:29 -0500

    Actualizando requirements y conectando la base de datos a MySQL

--- app_newspaper/app_newspaper/settings.py
+++ app_newspaper/app_newspaper/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'app_newspaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newspaper_db',
+        'USER': 'nico',
+        'PASSWORD': '6002meloNico*/*',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
